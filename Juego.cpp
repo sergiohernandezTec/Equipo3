f539703a2ecbd8058d90e9de53ed55e020024d5b
ayleen <A01426008@tec.mx>
2024-11-30 11:45:01 -0600

    hola

--- Juego.cpp
+++ Juego.cpp
@@ -1,2 +1,24 @@
 
 #include "Juego.h"
+
+
+
+Juego::Juego(string t,string g, int e) {
+    titulo = t;
+    genero = g;
+    edadMin = e;
+}
+
+
+
+void agregarPerfil(Perfil p);
+
+
+
+
+
+void mostrarPerfiles();
+vector listaPerfil();
+string getTitulo();
+string getGenero();
+int getEdadMin();
