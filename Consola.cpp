ecb34d548966854432c9a14407569a6ceab2543d
itzelbrito <A01425972@tec.mx>
2024-12-02 12:26:59 -0600

    h

--- Consola.cpp
+++ Consola.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Consola.h"
+
+Consola::Consola(string mod, string fab){
+  modelo = mod;
+  fabricante = fab;
+}
+
+bool Consola::encender(){
+ return true;
+}
+void Consola::descargarJuego(Juego j){
+  juegos.push_back(j);
+}
--- Consola.h
+++ Consola.h
@@ -2,21 +2,23 @@
 // Created by Itzelene Valladares Brito on 01/12/24.
 //
 #include <string>
+#include "Juego.h"
 using namespace std;
 #ifndef CONSOLA_H
 #define CONSOLA_H
+#include <vector>
 
 class Consola {
   private:
     string modelo;
     string fabricante;
-    Juego *juego;
+    vector <Juego> juegos;
+
   public:
     Consola(string mod, string fab);
-    void encender();
-    void descargarJuego(juego:Juego);
+    bool encender();
+    void descargarJuego(Juego j);
 };
-
 
 
 #endif //CONSOLA_H
